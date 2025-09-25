97e983f3d7ce5ede56b006aa46d1fea8e60067b0
Minh <phu.ngo@outlook.com>
2025-09-25 00:03:19 -0400

    convert webapp from git submodule to normal folder

---
+++ apps/web-app/src/routes/index.tsx
@@ -0,0 +1,42 @@
+import { createFileRoute } from '@tanstack/react-router'
+import logo from '../logo.svg'
+import { Button } from "@/components/ui/button"
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+//hello
+function App() {
+  return (
+    <div className="text-center">
+      <header className="min-h-screen flex flex-col items-center justify-center bg-background text-primary-foreground text-[calc(10px+2vmin)]">
+        <Button>Click me</Button>
+        <img
+          src={logo}
+          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
+          alt="logo"
+        />
+        <p>
+          Edit <code>src/routes/index.tsx</code> and save to reload.
+        </p>
+        <a
+          className="text-[#61dafb]` hover:underline"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="text-[#61dafb] hover:underline"
+          href="https://tanstack.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn TanStack
+        </a>
+
+      </header>
+    </div>
+  )
+}
---
+++ apps/web-app/vite.config.ts
@@ -0,0 +1,37 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+          target: 'react',
+          autoCodeSplitting: true,
+        }),    
+    viteReact(),
+    tailwindcss(),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+
+      '/api': {
+        target: 'http://localhost:3000/', 
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
